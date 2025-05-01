5088c48f4f26d3236f012f892052305830e74701
Ryan Richardson <116780532+rmrichardson88@users.noreply.github.com>
2025-05-01 15:07:24 -0600

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -14,7 +14,7 @@ def save_count(count):
         f.write(str(count))
 
 def update_readme(count):
-    message = f"I wrote a cheeky script that automatically contributes to my Github to illustrate why more than commits matter. This script has run {count} times."
+    message = f"I wrote a cheeky script that contributes to my Github by updating this ReadMe to illustrate why more than commits matter. This script has run {count} times."
     with open(README_PATH, "w") as f:
         f.write(message)
 
